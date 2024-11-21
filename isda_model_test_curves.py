2638c889d28e5873d40c5f5453a2900ae50183af
martyw <martin.witte@gmail.com>
2024-11-21 16:37:43 +0100

    Add files via upload

--- isda_model_test_curves.py
+++ isda_model_test_curves.py
@@ -25,7 +25,7 @@ class CreditCurvePoint(CurvePoint):
     survival_propability: float
 
     def __str__(self):
-        return ",".join([self.tenor.strftime(date_format)] + [str(x) for x in (self.survival_propability, self.year_fraction)])
+        return ",".join([self.tenor.strftime(date_format)] + [str(x) for x in (self.year_fraction, self.survival_propability)])
 
 @dataclass
 class IRCurvePoint(CurvePoint):
@@ -45,7 +45,6 @@ class Curve:
         self.object_name = object_name
         self.value_date = value_date
         self.tenors = tenors
-        self.value_date_jpmfmt = self.isda_dll.JpmcdsDate(self.value_date.year, self.value_date.month, self.value_date.day)
         if bad_day_convention not in ("N", "M", "F"):
             raise ValueError(f"Bad Bad Day Convention {bad_day_convention}, expected N, M, or F")
         self.bad_day_convention = ord(bad_day_convention)
@@ -64,10 +63,11 @@ class Curve:
 
         return interval
         
-    def forward_date_calculation(self, term):
+    def forward_date_calculation(self, from_date, term):
+        from_date_jpmfmt = self.isda_dll.JpmcdsDate(from_date.year, from_date.month, from_date.day)
         interval = self.convert_to_interval(term)
         dt = (ctypes.c_int * 1)()
-        self.isda_dll.JpmcdsDateFwdThenAdjust(self.value_date_jpmfmt, interval, self.bad_day_convention, self.holidays, dt)
+        self.isda_dll.JpmcdsDateFwdThenAdjust(from_date_jpmfmt, interval, self.bad_day_convention, self.holidays, dt)
         
         return dt[0]
         
@@ -88,8 +88,8 @@ class CreditCurve(Curve):
                 recovery_rate, cds_spreads):
         Curve.__init__(self, value_date, "CDSSpreadCurve", tenors, bad_day_convention, holidays)
         self.zero_curve = zero_curve[0]
-        self.stepin_date = self.forward_date_calculation("1D")
-        self.cash_settle_date = self.forward_date_calculation("3D")
+        self.stepin_date = self.forward_date_calculation(value_date, "1D")
+        self.cash_settle_date = self.forward_date_calculation(value_date, "3D")
         self.cds_spreads = (ctypes.c_double * len(cds_spreads))(*cds_spreads)
         self.recovery_rate = recovery_rate
         self.pay_accrual_on_default = pay_accrual_on_default
@@ -103,9 +103,9 @@ class CreditCurve(Curve):
         # tenors
         jpm_imm_dates = [self.isda_dll.JpmcdsDate(dt.year, dt.month,dt.day) for (_, dt) in self.tenors]
         tenors = (ctypes.c_int * len(jpm_imm_dates))(*jpm_imm_dates)
-        
-        credit_curve = self.isda_dll.JpmcdsCleanSpreadCurve(self.value_date_jpmfmt, self.zero_curve, \
-                                self.value_date_jpmfmt, self.stepin_date, self.cash_settle_date, len(jpm_imm_dates),\
+        value_date_jpmfmt = self.isda_dll.JpmcdsDate(self.value_date.year,self.value_date.month, self.value_date.day)
+        credit_curve = self.isda_dll.JpmcdsCleanSpreadCurve(value_date_jpmfmt, self.zero_curve, \
+                                value_date_jpmfmt, self.stepin_date, self.cash_settle_date, len(jpm_imm_dates),\
                                 tenors, self.cds_spreads, None, self.recovery_rate, self.pay_accrual_on_default, \
                                 self.coupon_interval, self.payment_dcc, self.stubFS, self.bad_day_convention, self.holidays)
         
@@ -128,18 +128,25 @@ class IRZeroCurve(Curve):
         self.floatleg_dcc = self.daycount_convention(floatleg_dcc)
         
     def build(self):            
-        # tenors
-        tenor_dates = []
-        for tenor in self.tenors:
-            tenor_dates.append(self.forward_date_calculation(tenor))
-        tenor_dates = (ctypes.c_int * len(tenor_dates))(*tenor_dates)
-                
-        zero_curve = self.isda_dll.JpmcdsBuildIRZeroCurve(self.value_date_jpmfmt, self.instrument_types, tenor_dates, self.rates, \
+        tenor_dates = self.setup_tenors()
+        value_date_jpmfmt = self.isda_dll.JpmcdsDate(self.value_date.year,self.value_date.month, self.value_date.day)
+        zero_curve = self.isda_dll.JpmcdsBuildIRZeroCurve(value_date_jpmfmt, self.instrument_types, tenor_dates, self.rates, \
                                                             len(self.instrument_types), self.fixedleg_freq, self.floatleg_freq, \
                                                             self.money_marketDCC, self.fixedleg_dcc, self.floatleg_dcc, \
                                                             self.bad_day_convention, self.holidays)
+        
+        self.set_result_curve(zero_curve)
+        return zero_curve
 
-        # add only points not on consecutive days
+    def setup_tenors(self): 
+        """ tenors to JPM format"""
+        tenor_dates = []
+        for tenor in self.tenors:
+            tenor_dates.append(self.forward_date_calculation(self.value_date, tenor))
+        return (ctypes.c_int * len(tenor_dates))(*tenor_dates)
+    
+    def set_result_curve(self, zero_curve):
+        """add only points not on consecutive days"""
         for i in range(zero_curve[0].fNumItems):
             (tenor, discount_factor) = self.get_tenor_and_value(zero_curve, i)
             if i == 0:
@@ -148,8 +155,7 @@ class IRZeroCurve(Curve):
                 self.result_curve.append(IRCurvePoint(value_date = self.value_date, tenor = tenor, discount_factor = discount_factor))
             else:
                 pass
-                    
-        return zero_curve
+    
                 
     def __str__(self):
         return "tenor,discount factor,YearFraction,Zero rate\n" + "\n".join([str(pt) for pt in self.result_curve])
@@ -162,6 +168,18 @@ class IRZeroCurve(Curve):
         
         return ctypes.c_long(int(freq_p[0]))
 
+class IRZeroCurveFromSpot(IRZeroCurve):
+        
+    def setup_tenors(self):
+        """ tenors to JPM format"""
+        tenor_dates = []
+        spot_date = self.forward_date_calculation(self.value_date, "2D")
+        spot_date = datetime.strptime("".join([chr(j) for j in list(self.isda_dll.JpmcdsFormatDate(spot_date))]), "%Y%m%d")
+        for tenor in self.tenors:
+            tenor_dates.append(self.forward_date_calculation(spot_date, tenor))
+        return (ctypes.c_int * len(tenor_dates))(*tenor_dates)
+                
+
 if __name__ == "__main__":    
     class TestCurveBuilders(unittest.TestCase):
         def setUp(self):
@@ -169,7 +187,7 @@ if __name__ == "__main__":
             open_gamma_market_data_example = [("M", "1M", 0.00445), ("M", "2M", 0.00949), ("M", "3M", 0.01234),\
                 ("M", "6M", 0.01776), ("M", "9M", 0.01935), ("M", "1Y", 0.02084), ("S", "2Y", 0.01652),\
                 ("S", "3Y", 0.02018), ("S", "4Y", 0.02303), ("S", "5Y", 0.02525), ("S", "6Y", 0.02696),\
-                ("S", "7Y", 0.02825), ("S", "8Y", 0.02931),("S", "9Y", 0.03017),("S", "10Y", 0.03092),\
+                ("S", "7Y", 0.02825), ("S", "8Y", 0.02931), ("S", "9Y", 0.03017), ("S", "10Y", 0.03092),\
                 ("S", "11Y", 0.03160),("S", "12Y", 0.03231),("S", "15Y", 0.03367), ("S", "20Y", 0.03419),\
                 ("S", "25Y", 0.03411),("S", "30Y", 0.03412)]
 
@@ -183,6 +201,13 @@ if __name__ == "__main__":
             zero_curve = self.zero_crv.build()
             self.zero_crv.write_to_file("zero_curve.csv")
             
+            # zero curve as in OG CDS pricing pdf
+            self.zero_crv_og = IRZeroCurveFromSpot(value_date, instrument_types, tenors, rates, "ACT/360", "6M", "3M", "30/360",  "ACT/360", "M", "None")
+            zero_curve_og = self.zero_crv_og.build()
+            self.zero_crv_og.write_to_file("zero_curve_opengamma.csv")
+
+            
+            # cds spread curve
             open_gamma_market_data_example = [("6M", "20/12/2011", 0.007927), ("1Y", "20/06/2012", 0.007927), \
                                               ("3Y", "20/06/2014", 0.012239), ("5Y", "20/06/2016", 0.016979), \
                                               ("7Y", "20/06/2018", 0.019271), ("10Y", "20/06/2021", 0.02086)]
